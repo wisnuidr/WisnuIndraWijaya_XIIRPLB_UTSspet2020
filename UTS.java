326b40d01ced76d6e4046d1cf1dcaa13d84a4bb2
wisnuidr <64117172+wisnuidr@users.noreply.github.com>
2020-09-09 09:38:55 +0700

    Add files via upload

---
+++ UTS.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class UTS {
+
+	public static void main(String[] args) {
+		String wisnu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-# ";
+		char name1 = wisnu.charAt(22);
+		char name2 = wisnu.charAt(8);
+		char name3 = wisnu.charAt(18);
+		char name4 = wisnu.charAt(13);
+		char name5 = wisnu.charAt(20);
+		
+		char name6 = wisnu.charAt(8);
+		char name7 = wisnu.charAt(13);
+		char name8 = wisnu.charAt(3);
+		char name9 = wisnu.charAt(17);
+		char name10 = wisnu.charAt(0);
+		
+		System.out.println("" + name1 + name2 + name3 + name4 + name5 + " " 
+		+ name6 + name7 + name8 + name9 + name10);
+
+	}
+
+}
